9a7afcadc283752bbb0a4768443daac747d32186
Jakub Stachowski <jakub@augnet.co.uk>
2019-08-12 22:18:21 +0200

    Return list of UICC packages - needed for CarrierMessagingService

--- app/src/main/java/com/augnet/uiccbypass/UICCModule.java
+++ app/src/main/java/com/augnet/uiccbypass/UICCModule.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.telephony.TelephonyManager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -16,6 +17,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 
 public class UICCModule implements IXposedHookLoadPackage {
+    private final static String DEFAULT_CARRIER_APP_PACKAGE="com.augnet.uiccservice";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.android.phone"))
@@ -28,7 +31,22 @@ public class UICCModule implements IXposedHookLoadPackage {
                         return (int)1;
                     }
                 });
+
+        XposedHelpers.findAndHookMethod("com.android.internal.telephony.uicc.UiccCard", lpparam.classLoader,
+                "getCarrierPackageNamesForIntent", PackageManager.class, Intent.class, new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("Called getCarrierPackageNamesForIntent for "+param.args[1].toString());
+                        List<String> ret = new ArrayList<>();
+                        ret.add(DEFAULT_CARRIER_APP_PACKAGE);
+                        return ret;
+                    }
+                });
+
         XposedBridge.log("UICC privileges bypass installed on "+lpparam.packageName);
+
+
+
     }
 
 
